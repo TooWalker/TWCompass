b291b60f5ff52b01f9bde30e41d028ef7dbc61ba
lei.dai <lei.dai@qunar.com>
2016-06-19 11:34:20 +0800

    add: 创建工程，初步配置

---
+++ TWCompass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TWCompass
+//
+//  Created by TooWalker on 6/19/16.
+//  Copyright © 2016 TooWalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
